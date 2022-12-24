8ac7f110a2d856a421750610f1959c7d5915662a
verrelio.cr <verrelioragna@gmail.com>
2022-12-24 14:06:27 +0700

    [Verrel] create model for list movie

---
+++ Movie Directory/Modules/List Movie/ListMovieModel.swift
@@ -0,0 +1,16 @@
+//
+//  ListMovieModel.swift
+//  Movie Directory
+//
+//  Created by Verrelio C. Rizky on 24/12/22.
+//
+
+import Foundation
+
+class ListMovieModel {
+    var page: Int = 1
+    var totalPages: Int = 0
+    var totalElements: Int = 0
+    var allowRequestNextPage: Bool = true
+    var movies: [ListMovieObject] = []
+}
